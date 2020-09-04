7ff0d0cdd7f31b3307036f73fde7ec4a477719b4
Sebastian Olsson <olsson.sebastian.87@gmail.com>
2020-09-04 09:48:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace NumberGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+            Console.Write("Please pick a number: ");
+            
+            int number = int.Parse(Console.ReadLine());
+
+            const int correctNumber = 42;
+            
+
+
+            if (correctNumber == number)
+            {
+                Console.WriteLine("Congratulations, your number is correct.");
+                    break;
+            }
+            else if (correctNumber > number)
+            {
+                Console.WriteLine("I'm sorry, your number is to low.");
+            }
+            else if (correctNumber < number)
+            {
+                Console.WriteLine("I'm sorry, your number is to high.");
+            }
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
